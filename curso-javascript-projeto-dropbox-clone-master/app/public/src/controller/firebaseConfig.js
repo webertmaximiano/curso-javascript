52c28ba0b039a09221c9feb291c7316f97f23bcb
Webert Maximiano <webertcoach@gmail.com>
2023-07-20 11:48:56 -0300

    iniciando projeto landing-page

---
+++ curso-javascript-projeto-dropbox-clone-master/app/public/src/controller/firebaseConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCGyqIw5RWe6T1gNzm_D8wSOvhgC8alo5Y",
+  authDomain: "dropbox-clone-d30e1.firebaseapp.com",
+  projectId: "dropbox-clone-d30e1",
+  storageBucket: "dropbox-clone-d30e1.appspot.com",
+  messagingSenderId: "409135357703",
+  appId: "1:409135357703:web:ccaa996b5a826864522fa2",
+  measurementId: "G-X4TYZXY51G"
+};
--- curso-javascript-projeto-dropbox-clone-master/app/public/src/index.js
+++ curso-javascript-projeto-dropbox-clone-master/app/public/src/index.js
@@ -1,1 +1,34 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+//import { getFirestore, collection, getDocs, getDoc } from 'firebase/firestore';
+//import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseApp = initializeApp ({
+  apiKey: "AIzaSyCGyqIw5RWe6T1gNzm_D8wSOvhgC8alo5Y",
+  authDomain: "dropbox-clone-d30e1.firebaseapp.com",
+  databaseURL: "https://dropbox-clone-d30e1-default-rtdb.firebaseio.com",
+  projectId: "dropbox-clone-d30e1",
+  storageBucket: "dropbox-clone-d30e1.appspot.com",
+  messagingSenderId: "409135357703",
+  appId: "1:409135357703:web:ccaa996b5a826864522fa2",
+  measurementId: "G-X4TYZXY51G"
+});
+
+const auth = getAuth(firebaseApp);
+//const db = getFirestore(firebaseApp);
+//db.collection('todos').getDocs;
+//const todosCol = collection(db, 'todos');
+//const snapshot = await getDocs(todosCol);
+
+//detect auth state
+onAuthStateChanged(auth, user => {
+    if (user != null) {
+        console.log('logged in!');
+    } else {
+        console.log('No user');
+    }
+});
+
+
 window.app = new DropBoxController();
